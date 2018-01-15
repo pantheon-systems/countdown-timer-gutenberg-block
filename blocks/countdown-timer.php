ee7f8e632af3d93804458feecd2083031d527cd1
Daniel Bachhuber <daniel@handbuilt.co>
2018-01-15 05:47:35 -0800

    Rename to better match naming standards

--- blocks/countdown-timer.php
+++ blocks/countdown-timer.php
@@ -2,13 +2,13 @@
 
 function countdown_timer_enqueue_block_editor_assets() {
 	$dir = dirname( __FILE__ );
-	$block_js = 'countdown-timer/block.js';
+	$index_js = 'countdown-timer/index.js';
 	$editor_css = 'countdown-timer/editor.css';
-	wp_enqueue_script( 'countdown-timer-block', plugins_url( $block_js, __FILE__ ), array(
+	wp_enqueue_script( 'countdown-timer-index_js', plugins_url( $index_js, __FILE__ ), array(
 		'wp-blocks',
 		'wp-i18n',
 		'wp-element',
-	), filemtime( "$dir/$block_js" ) );
+	), filemtime( "$dir/$index_js" ) );
 	wp_enqueue_style( 'countdown-timer-block', plugins_url( $editor_css, __FILE__ ), array(
 		'wp-blocks',
 	), filemtime( "$dir/$editor_css" ) );
